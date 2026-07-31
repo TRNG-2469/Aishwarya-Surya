aaa8206993168ecd61ba19ceecd449111b0e8995
snsorl <145521182+snsorl@users.noreply.github.com>
2026-07-31 14:43:06 -0500

    user

--- src/main/java/com/aishwarya/ers/model/User.java
+++ src/main/java/com/aishwarya/ers/model/User.java
@@ -1,4 +1,73 @@
 package com.aishwarya.ers.model;
 
 public class User {
+
+    private int id;
+    private String username;
+    private String password; 
+    private Role role;
+    private String department;
+
+    // Constructors
+
+    public User() {
+    }
+
+    public User(int id, String username, String password, Role role, String department) {
+        this.id = id;
+        this.username = username;
+        this.password = password;
+        this.role = role;
+        this.department = department;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public Role getRole() {
+        return role;
+    }
+
+    public String getDepartment() {
+        return department;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public void setRole(Role role) {
+        this.role = role;
+    }
+
+    public void setDepartment(String department) {
+        this.department = department;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", username='" + username + '\'' +
+                ", role=" + role +
+                ", department='" + department + '\'' +
+                '}';
+    }
 }
